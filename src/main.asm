e60d331bce70579ccb26cf5b1797b0e435e582d5
nssSSH <218080797+nssSSH@users.noreply.github.com>
2026-01-02 13:18:16 +0530

    It boots and it prints to the screen, but no ASCII art for now.

--- src/main.asm
+++ src/main.asm
@@ -1,13 +1,54 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+start:
+  jmp main
+; prints a string to the screen
+
+puts:
+ push si
+ push ax
+
+
+.loop:
+  lodsb 
+  or al, al
+  jz .done
+  
+  mov ah, 0x0e
+  mov bh, 0
+  int 0x10
+  jmp .loop
+
+.done:
+  pop ax
+  pop si
+  ret
+
+
 
 main:
+ ; setup data segments
+ mov ax, 0 
+ mov ds, ax
+ mov es, ax
+
+  ; setup stack
+  mov ss, ax
+  mov sp, 0x7C00
+  
+  mov si, msg_hello
+  call puts
+
+
  hlt
 
 .halt:
   jmp .halt
   
+msg_hello: db "Hello world from NSGK!", ENDL
+  db "I wanted to add ASCII art but it was too difficult!!!", ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
